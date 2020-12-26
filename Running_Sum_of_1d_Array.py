8432cf78e426eb026b6ebab8cc8f6ea6c8d46e1e
myamaak <62881169+myamaak@users.noreply.github.com>
2020-12-26 15:54:05 +0900

    Update Running_Sum_of_1d_Array.py

--- Running_Sum_of_1d_Array.py
+++ Running_Sum_of_1d_Array.py
@@ -2,8 +2,9 @@ import numpy as np
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
         out=[]
+        this=0
         leng = len(nums)
         for i in range(leng):
-            this = nums[:i+1]
+            this += nums[i]
             out.append(np.sum(this))
         return out
