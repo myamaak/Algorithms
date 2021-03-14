169c29a24b41049657ca3241a596694789f6f6de
Nam Dayoung <ramda4415@gmail.com>
2021-03-14 09:38:57 +0900

    0314

--- MaxSliceSum.py
+++ MaxSliceSum.py
@@ -8,7 +8,7 @@ def getSubsum(data) :
         
     return max_value
 
-#divide-and-conquer
+#divide-and-conquer -> O(nlogn)
 def getSubsum(data) :
 
     n = len(data)
@@ -37,11 +37,29 @@ def getSubsum(data) :
     
     return max([left, right, leftSum+rightSum])
 
-#dynamic programming
+#dynamic programming -> O(n)
+
+#f(i) = 인덱스 i를 오른쪽 끝으로 갖는 구간의 최대합
+#f(i) = max(0, f(i-1))+arr[i]   (if i >0)
+#     = arr[i]                  (if i==0)
+
 def getSubsum(data) :
 
-    dp = [i for i in data]
-    for i in range(1, len(data)):
+    dp = [i for i in data] 
+    for i in range(1, len(data)): #f(0) = arr[0] 이므로 1부터 시작한다.
         dp[i] = max(dp[i-1] + data[i], data[i])
+        #-> dp[i] = max(0, dp[i-1]) + data[i] 과 동일하다.
     
     return max(dp)
+
+#바보풀이
+# def getSubsum(data) :
+#     dp =[]
+#     n = len(data)
+    
+#     for i in range(n):
+#         max_sum = 0
+#         for j in range(i,n+1):
+#             max_sum = max(max_sum, sum(data[i:j]))
+#         dp.append(max_sum)
+#     return max(dp)
